e7c9e114c12b5f8c37617e3e345e1c4596e88619
Rico Borghs <rico.borghs@student.howest.be>
2017-11-18 19:10:30 +0100

    Form validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,29 @@
+const blurListener = (e) => {
+  const field = e.target;
+  let message = "";
 
+  if (field.value == "") {
+    message = "Verplicht veld";
+  }
+
+  field.parentNode.querySelector('.feedback').textContent = message;
+  field.classList.add("error");
+}
+
+const focusListener = (e) => {
+  const field = e.target;
+
+  field.parentNode.querySelector('.feedback').textContent = "";
+  field.classList.remove("error");
+}
 
 const init = () => {
-
-  console.log(`Hello, Babeleir-de-bretagne`);
-
+  console.log("test");
+  document.querySelectorAll(".form-field input").forEach(field => {
+    console.log(field);
+    field.addEventListener('blur', blurListener);
+    field.addEventListener('focus', focusListener);
+  });
 };
 
 init();
